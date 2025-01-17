ccfe7c0bf5e4abd4e94e4d939778a3204ae7cd35
Taki Do <ferrusi.enzo06@gmail.com>
2025-01-17 08:55:44 +0100

    search

--- main.go
+++ main.go
@@ -1,17 +1,49 @@
 package main
 
 import (
+	"encoding/json"
 	"html/template"
 	"net/http"
+	"strings"
 )
 
-func start(w http.ResponseWriter, r *http.Request) {
-	tmpl, _ := template.ParseFiles("templates/search.html")
-	tmpl.Execute(w, nil)
+type PageData struct {
+	Groups []GroupInfos
+}
+type GroupInfos struct {
+	ID           int      `json:"id"`
+	Image        string   `json:"image"`
+	Name         string   `json:"name"`
+	Members      []string `json:"members"`
+	CreationDate int      `json:"creationDate"`
+	FirstAlbum   string   `json:"firstAlbum"`
+	Locations    string   `json:"locations"`
+	ConcertDates string   `json:"concertDates"`
+	Relations    string   `json:"relations"`
 }
 
-func searchFunc(input string) {
+func start(w http.ResponseWriter, r *http.Request) {
+	resp, _ := http.Get("https://groupietrackers.herokuapp.com/api/artists")
+	var infos []GroupInfos
+	json.NewDecoder(resp.Body).Decode(&infos)
+	if r.Method == http.MethodPost {
+		infos = searchFunc(r.FormValue("userInput"), infos)
+	}
+	gg := PageData{
+		Groups: infos,
+	}
+	tmpl, _ := template.ParseFiles("templates/search.html")
+	tmpl.Execute(w, gg)
+}
 
+func searchFunc(input string, infos []GroupInfos) []GroupInfos {
+	var output []GroupInfos
+	for i := 0; i < len(infos); i++ {
+		if strings.Contains(infos[i].Name, input) {
+			output = append(output, infos[i])
+		}
+	}
+	return output
 }
 
 func main() {
